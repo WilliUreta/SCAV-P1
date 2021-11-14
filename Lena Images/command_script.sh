a03a655aa6757fdad983ce2bf68cd5f6397ddc39
WilliUreta <62305530+WilliUreta@users.noreply.github.com>
2021-11-14 17:51:46 +0100

    Update command_script.sh

--- Lena Images/command_script.sh
+++ Lena Images/command_script.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+#Resize image to lower quality
+ffmpeg -i Lenna_\(test_image\).jpeg -vf scale=320:240,setsar=1:1 320_240_lena.jpeg
+
+#Transform image to b/w with hardest compression
+ffmpeg -i Lenna_\(test_image\).jpeg -vf format=gray -qscale:v 31 bw_lena.jpeg
